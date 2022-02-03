49409253a9e71323268cac143fbad7aa08f222d8
zotovalexrew <che123zotov@gmail.com>
2022-02-03 19:52:09 +0300

    Новый файл js

---
+++ newScript.js
@@ -0,0 +1,58 @@
+"use strict";
+
+//Проверка на число
+const isNumber = function(num) {
+ return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+//Игра в угадайку
+
+function gameBot()
+{
+ //Задание переменных
+ let attempts = 10;
+ let number = prompt(`Угадай число от 1 до 100. Количество попыток: ${attempts}`);
+ let hiddenNumber = Math.ceil(Math.random() * 100);
+
+  //Проверка условий
+  function answer()
+  {
+    //Если попытки закончились
+    if (attempts <= 1)
+    {
+      number = alert("Попытки закончились, хочешь сыграть еще?");
+    }
+    else
+    {
+      //Проверка введенного числа с загаднным числом
+      if (number === null)
+      {
+        number = alert("Игра окончена");
+      }
+      else if (!isNumber(number)) //Если введена фраза а не число
+      {
+        number = prompt("Введите число!");
+        answer();
+      }
+      else if (number == hiddenNumber)
+      {
+        number = alert("Поздравляю, ты угадал! Хочещь сыграть еще?");
+      }
+      else if (number > hiddenNumber)
+      {
+        attempts--;
+        number = prompt(`Загаданное число меньше! Попробуй еще раз! Осталось попыток: ${attempts}`);
+        answer();
+      }
+      else if (number < hiddenNumber) 
+      {
+        attempts--;
+        number = prompt(`Загаданное число больше! Попробуй еще раз! Осталось попыток: ${attempts}`);
+        answer();
+      }
+    }
+  }
+  answer();
+
+}
+gameBot();
